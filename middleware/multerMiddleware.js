8d8f69f7eb1f25bf3716e089aef290e95d5f9877
athul200325 <athul250703@gmail.com>
2024-12-05 23:10:21 +0530

    final change

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,15 +1,13 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './uploads'); // Set the upload directory
+const storage=multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null,'./uploads')
     },
-    filename: (req, file, cb) => {
-        // Replace spaces in the filename and remove space between "image" and timestamp
-        const sanitizedFilename = file.originalname.replace(/\s+/g, '-');
-        cb(null, `image-${Date.now()}-${sanitizedFilename}`);
+    filename: (req,file,cb)=>{
+        cb(null,`image-${Date.now()}-${file.originalname}`)
     }
-});
+})
 
 const multerMiddleware=multer({storage})
 
